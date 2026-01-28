0b3bbcb1923438033b0d0e8ca818389d22fcf41b
parishek <parishekbhai716@gmail.com>
2026-01-28 20:59:11 +0530

    ssl certificate disable

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -4,7 +4,7 @@ set -e
 echo "Waiting for database at $DB_HOST:$DB_PORT..."
 
 # Loop until MySQL is ready
-while ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" --ssl-ca="$MYSQL_ATTR_SSL_CA" -e "SELECT 1" &> /dev/null
+while ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" --ssl-mode=DISABLED -e "SELECT 1" &> /dev/null
 do
   echo "Database not ready yet, sleeping 2 seconds..."
   sleep 2
